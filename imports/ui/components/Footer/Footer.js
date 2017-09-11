0be4d4aded13e368d065bba3cf4027597aa3243e
Amr Abdullah <abu.muslim.amr@gmail.com>
2017-09-11 17:58:05 +0200

    3. Change the Footer so it states your name and the date

--- imports/ui/components/Footer/Footer.js
+++ imports/ui/components/Footer/Footer.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { year } from '@cleverbeagle/dates';
+import { monthDayYear } from '@cleverbeagle/dates';
 import { Link } from 'react-router-dom';
 import { Grid } from 'react-bootstrap';
 
 import './Footer.scss';
 
-const copyrightYear = () => {
-  const currentYear = year();
-  return currentYear === '2017' ? '2017' : `2017-${currentYear}`;
-};
+const currentDate = () => monthDayYear();
 
 const Footer = () => (
   <div className="Footer">
     <Grid>
-      <p className="pull-left">&copy; {copyrightYear()} Application Name</p>
+      <p className="pull-left">Amr Abdullah | {currentDate()}</p>
       <ul className="pull-right">
         <li><Link to="/terms">Terms<span className="hidden-xs"> of Service</span></Link></li>
         <li><Link to="/privacy">Privacy<span className="hidden-xs"> Policy</span></Link></li>
