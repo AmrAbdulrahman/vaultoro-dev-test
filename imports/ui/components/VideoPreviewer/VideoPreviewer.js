5192b9f95c62e00c8366954bf9954e5c2b935ece
Amr Abdullah <abu.muslim.amr@gmail.com>
2017-09-12 22:13:34 +0200

    10.c. Add YouTube Player to the List, View, Add, and Edit Video Pages

--- imports/ui/components/VideoPreviewer/VideoPreviewer.js
+++ imports/ui/components/VideoPreviewer/VideoPreviewer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import YouTube from 'react-youtube';
 
 const DEFAULT_URL_VALUE = '';
 
@@ -9,46 +10,109 @@ class VideoPreviewer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      url: props.url,
-      error: false,
-    };
-  }
+    this.state = {};
 
-  onLoadingError(err) {
-    this.setState({
-      error: true,
-    });
+    setTimeout(() => this.loadVideo(props.url));
   }
 
   componentDidUpdate(props) {
     if (props.url !== this.props.url) {
-      this.setState({
-        error: false,
-        url: this.props.url,
-      });
+      this.loadVideo(this.props.url);
+    }
+
+    if (this.props.paused) {
+      this.pause();
     }
   }
 
-  render() {
-    return (
-      <div className='VideoPreviewer'>
-        {(() => {
-          if (this.state.url === DEFAULT_URL_VALUE) {
-            return (
-              <div>No url set.</div>
-            );
-          }
+  loadVideo(url) {
+    this.setState({
+      videoId: this.getVideoID(url),
+    });
+  }
 
-          if (this.state.error === true) {
+  pause() {
+    // pause the video of we already loaded and we have a reference
+    this.videoReference && this.videoReference.pauseVideo();
+  }
+
+  // trim other query parameters like 'time=X'
+  // sometimes, we have it as a second parameter '&t=123'
+  // and sometimes, we have it as first param '?t=123'
+  // @TODO this should get handled automatically by Google
+  // to accept a videoUrl and not only an ID, check newer versions.
+  trimYoutubeUrlParameters(url) {
+    const ampersandPosition = url.indexOf('&');
+    const questionMarkPosition = url.indexOf('?');
+
+    if (ampersandPosition !== -1) {
+      return url.substring(0, ampersandPosition);
+    } else if (questionMarkPosition !== -1) {
+      return url.substring(0, questionMarkPosition);
+    }
+
+    return url;
+  }
+
+  getVideoID(url) {
+    try {
+      const shortLinkBase = 'https://youtu.be/';
+      const isShortLink = url.indexOf(shortLinkBase) !== -1;
+      let videoId = null;
+
+      if (isShortLink === true) {
+        videoId = url.replace(shortLinkBase, '');
+      } else {
+        videoId = url.split('v=')[1];
+      }
+
+      return this.trimYoutubeUrlParameters(videoId);
+    } catch (ex) {
+      return false; // raises the error message
+    }
+  }
+
+  onPlayerError() {
+    this.setState({
+      videoId: false,
+    });
+  }
+
+  // hold a reference to the video so we can manually
+  // pause it later
+  onPlayerReady(event) {
+    this.videoReference = event.target;
+
+    // check if should be forced as paused.
+    if (this.props.paused) {
+      this.pause();
+    }
+  }
+
+  // propagate onPlay event
+  onPlayerPlay(event) {
+    this.props.onPlay && this.props.onPlay(event);
+  }
+
+  render() {
+    const cssClass = 'VideoPreviewer' + (this.props.inline ? ' inline' : '');
+
+    return (
+      <div className={cssClass}>
+        {(() => {
+          if (this.state.videoId === false) {
             return (
-              <div>No preview available, invalid url.</div>
+              <div>No preview available, url is invalid or missing.</div>
             );
           }
 
           return (
             <div>
-              <img src={this.state.url} onError={(err) => this.onLoadingError(err)}/>
+              <YouTube
+                videoId={this.state.videoId}
+                onError={() => this.onPlayerError()}
+                onReady={(event) => this.onPlayerReady(event)}
+                onPlay={(event) => this.onPlayerPlay(event)} />
             </div>
           );
         })()}
@@ -59,10 +123,14 @@ class VideoPreviewer extends React.Component {
 
 VideoPreviewer.defaultProps = {
   url: DEFAULT_URL_VALUE,
+  inline: false,
+  paused: false,
 };
 
 VideoPreviewer.propTypes = {
   url: PropTypes.string,
+  inline: PropTypes.bool,
+  paused: PropTypes.bool,
 };
 
 export default VideoPreviewer;
--- imports/ui/pages/Videos/Videos.js
+++ imports/ui/pages/Videos/Videos.js
@@ -25,56 +25,80 @@ const handleRemove = (videoId) => {
   }
 };
 
-const Videos = ({ loading, videos, match, history }) => (!loading ? (
-  <div className="Videos">
-    <div className="page-header clearfix">
-      <h4 className="pull-left">Videos</h4>
-      <Link className="btn btn-success pull-right" to={`${match.url}/new`}>Add Video</Link>
-    </div>
-    {videos.length ? <Table responsive>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Last Updated</th>
-          <th>Created</th>
-          <th>Preview</th>
-          <th>Rating</th>
-          <th />
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        {videos.map(({ _id, title, url, rating, createdAt, updatedAt }) => (
-          <tr key={_id}>
-            <td>{title}</td>
-            <td>{timeago(updatedAt)}</td>
-            <td>{monthDayYearAtTime(createdAt)}</td>
-            <td className='previewCol'>
-              <VideoPreviewer url={url} />
-            </td>
-            <td>
-              <Rating value={rating} />
-            </td>
-            <td>
-              <Button
-                bsStyle="primary"
-                onClick={() => history.push(`${match.url}/${_id}`)}
-                block
-              >View</Button>
-            </td>
-            <td>
-              <Button
-                bsStyle="danger"
-                onClick={() => handleRemove(_id)}
-                block
-              >Delete</Button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table> : <Alert bsStyle="warning">No Videos yet!</Alert>}
-  </div>
-) : <Loading />);
+class Videos extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPlayingId: null,
+    };
+  }
+
+  setCurrentPlayingId(id) {
+    this.setState({
+      currentPlayingId: id,
+    });
+  }
+
+  render() {
+    const { loading, videos, history, match } = this.props;
+
+    return (!loading ? (
+      <div className="Videos">
+        <div className="page-header clearfix">
+          <h4 className="pull-left">Videos</h4>
+          <Link className="btn btn-success pull-right" to={`${match.url}/new`}>Add Video</Link>
+        </div>
+        {videos.length ? <Table responsive>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Last Updated</th>
+              <th>Created</th>
+              <th>Preview</th>
+              <th>Rating</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {videos.map(({ _id, title, url, rating, createdAt, updatedAt}) => (
+              <tr key={_id}>
+                <td>{title}</td>
+                <td>{timeago(updatedAt)}</td>
+                <td>{monthDayYearAtTime(createdAt)}</td>
+                <td className='previewCol'>
+                  <VideoPreviewer
+                    url={url}
+                    inline={true}
+                    paused={this.state.currentPlayingId !== _id}
+                    onPlay={() => this.setCurrentPlayingId(_id)} />
+                </td>
+                <td>
+                  <Rating value={rating} />
+                </td>
+                <td>
+                  <Button
+                    bsStyle="primary"
+                    onClick={() => history.push(`${match.url}/${_id}`)}
+                    block
+                  >View</Button>
+                </td>
+                <td>
+                  <Button
+                    bsStyle="danger"
+                    onClick={() => handleRemove(_id)}
+                    block
+                  >Delete</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table> : <Alert bsStyle="warning">No Videos yet!</Alert>}
+      </div>
+    ) : <Loading />);
+  }
+}
 
 Videos.propTypes = {
   loading: PropTypes.bool.isRequired,
